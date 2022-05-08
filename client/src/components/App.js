da3c6f4ec6924edecfdafb6dc82de773a5a03d42
Shaun Robinson <shaunrobinson@Shauns-MacBook-Air-2.local>
2022-05-07 19:49:00 -0700

    Create BioDivs Component and rough CSS

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import '../styles/App.scss';
 import Nav from './Nav';
 import Bio from './Bio';
+import BioDivs from './BioDivs';
 
 export default function App() {
   return (
     <>
     <Nav/>
     <Bio/>
+    <BioDivs/>
     </>
   );
 }
