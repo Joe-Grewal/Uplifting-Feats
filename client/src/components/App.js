2d4cd5a9ae414a0ba5a78aed7c830ba75c80a3af
Shaun Robinson <shaunrobinson@Shauns-MacBook-Air-2.local>
2022-05-08 11:52:10 -0700

    Add MyHealthJournal Component with rough CSS

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,7 @@ import '../styles/App.scss';
 import Nav from './Nav';
 import Bio from './Bio';
 import BioDivs from './BioDivs';
+import MyHealthJournal from './MyHealthJournal';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
     <Nav/>
     <Bio/>
     <BioDivs/>
+    <MyHealthJournal/>
     </>
   );
 }
