f4cf5648e93e2a0c9937a40af6b2ef611939b29a
Abby <abby_faigao@yahoo.com>
2022-05-09 09:33:25 +0000

    merge

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -3,28 +3,24 @@ import React from "react";
 export default function Nav() {
   return (
     <header className="nav_container">
-    <nav className="navigation">
-<<<<<<< HEAD
-      <div className="navlogo">
-        <a href="/home">Uplifting Feats</a>
-      </div>
-      <div className="navlinks">
-        <a href="/home">Search</a>
-        <a href="/My_Profile">My Profile</a>
-        <a href="/login">Login</a>
-      </div>
-    </nav>
+      <nav className="navigation">
+        <div className="logo">
+          <a className="logo_link" href="/home">
+            Uplifting Feats
+          </a>
+        </div>
+        <div className="navlinks">
+          <a className="right_links" href="/home">
+            Search
+          </a>
+          <a className="right_links" href="/My_Profile">
+            My Profile
+          </a>
+          <a className="right_links" href="/login">
+            Login
+          </a>
+        </div>
+      </nav>
+    </header>
   );
 }
-=======
-      <div className="logo"><a className="logo_link" href="/home">Uplifting Feats</a></div>
-      <div className="navlinks">
-        <a className="right_links" href="/home">Search</a>
-        <a className="right_links" href="/My_Profile">My Profile</a>
-        <a className="right_links" href="/login">Login</a>
-      </div>
-    </nav>
-    </header>
-  )
-}
->>>>>>> 66850f95b2a0feb0659dd89741824d2408d1a673
