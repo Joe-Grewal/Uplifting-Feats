2380d54f05c94a69b74aa58941548bbb1cb7af74
Shaun Robinson <shaunrobinson@Shauns-MacBook-Air-2.local>
2022-05-15 13:55:22 -0700

    Create a single page for journal entries

---
+++ client/src/pages/JournalEntries.js
@@ -0,0 +1,34 @@
+import React from "react";
+import EntryCards from "../components/EntryCards";
+import EntryCards_NOT_logged_in from "../components/EntryCards_NOT_logged_in";
+import BBackToProfile from "../components/BBackToProfile";
+
+
+
+export default function Search () {
+  return (
+    <>
+
+    {/* if logged in / my journal entries */}
+      <div className="search_page_top_copy_container">
+        <h3>Your Journal Entries</h3>
+        <p>Search through and edit or delete past entries</p>
+      </div>
+      <EntryCards/>
+
+
+      {/* other users entries / not mine */}
+      <div className="search_page_top_copy_container">
+        <h3>Journal Entries</h3>
+        <p>Here is a listing of all the stories from your chosen health enthusiast. Happy reading!</p>
+      </div>
+
+      <EntryCards_NOT_logged_in/>
+      
+      <div className="button_spacing_container">
+        <BBackToProfile />
+      </div>
+
+    </>
+  );
+}
