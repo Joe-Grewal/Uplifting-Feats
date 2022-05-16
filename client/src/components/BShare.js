2595433bfc502c638b2388c5d4aa110808df40e2
Abby <abby_faigao@yahoo.com>
2022-05-16 08:20:06 +0000

    Install and use react-share

--- client/src/components/BShare.js
+++ client/src/components/BShare.js
@@ -1,39 +1,74 @@
-import React from 'react';
+import React from "react";
 import "../styles/App.scss";
-import { Button, createTheme } from '@mui/material';
-import { ThemeProvider } from '@emotion/react';
+import { Button, createTheme } from "@mui/material";
+import { ThemeProvider } from "@emotion/react";
 import Share from "../images/share.svg";
 
-export default function BShare () {
-  const theme = createTheme ({
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  EmailShareButton,
+  EmailIcon,
+  TwitterShareButton,
+  TwitterIcon,
+} from "react-share";
+
+export default function BShare() {
+  const shareUrl = "https://localhost:3000/";
+
+  const theme = createTheme({
     typography: {
-      fontFamily: [
-        'Quicksand', 'sans-serif',
-      ].join(','),
+      fontFamily: ["Quicksand", "sans-serif"].join(","),
       fontSize: 25,
-      fontWeightMedium:700,
+      fontWeightMedium: 700,
     },
     palette: {
       primary: {
-        main: '#6A18A8'
-      }
+        main: "#6A18A8",
+      },
     },
     shadows: {
-      0: "none"
-    }
+      0: "none",
+    },
   });
 
   return (
-    <ThemeProvider theme={theme}>
-      <div className="large_share_button">
-      <Button id="share_button"
-      onClick={() => {
-      alert('clicked');
-      }}
-      variant="text"><p className="large_share_text">
-        SHARE</p> <img src={Share} alt="share" className="share_image_large"/>
-    </Button>
-    </div>
-  </ThemeProvider>
-  )
+    <>
+      <FacebookShareButton
+        url={shareUrl}
+        quote="Uplifting Feats"
+        hashtag="#UpliftingFeats"
+      >
+        <FacebookIcon size={32} round />
+      </FacebookShareButton>
+
+      <EmailShareButton
+        url={shareUrl}
+        quote="Uplifting Feats"
+        hashtag="#UpliftingFeats"
+      >
+        <EmailIcon size={32} round />
+      </EmailShareButton>
+
+      <TwitterShareButton
+        url={shareUrl}
+        quote="Uplifting Feats"
+        hashtag="#UpliftingFeats"
+      >
+        <TwitterIcon size={32} round />
+      </TwitterShareButton>
+    </>
+
+    //   <ThemeProvider theme={theme}>
+    //     <div className="large_share_button">
+    //     <Button id="share_button"
+    //     onClick={() => {
+    //     alert('clicked');
+    //     }}
+    //     variant="text"><p className="large_share_text">
+    //       SHARE</p> <img src={Share} alt="share" className="share_image_large"/>
+    //   </Button>
+    //   </div>
+    // </ThemeProvider>
+  );
 }
--- client/src/components/Share_small.js
+++ client/src/components/Share_small.js
@@ -1,20 +1,53 @@
 import React from "react";
 import Share from "../images/share.svg";
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  EmailShareButton,
+  EmailIcon,
+  TwitterShareButton,
+  TwitterIcon,
+} from "react-share";
 
-export default function Share_small () {
-
+export default function Share_small() {
+  const shareUrl = "https://localhost:3000/";
   return (
+    <>
+      <FacebookShareButton
+        url={shareUrl}
+        quote="Uplifting Feats"
+        hashtag="#UpliftingFeats"
+      >
+        <FacebookIcon size={32} round />
+      </FacebookShareButton>
 
-    <div className="like_small_container">
-        
-        <div className="small_share_image">
-        <img src={Share} alt="share"/>
-        </div>
+      <EmailShareButton
+        url={shareUrl}
+        quote="Uplifting Feats"
+        hashtag="#UpliftingFeats"
+      >
+        <EmailIcon size={32} round />
+      </EmailShareButton>
 
-        <div className="number_of_likes">
-        <p className="num_of_shares">12345<span className="likes_pink"> SHARES</span></p>
-        </div>
+      <TwitterShareButton
+        url={shareUrl}
+        quote="Uplifting Feats"
+        hashtag="#UpliftingFeats"
+      >
+        <TwitterIcon size={32} round />
+      </TwitterShareButton>
+    </>
 
-   </div> 
-  )
+    //   <div className="like_small_container">
+
+    //       <div className="small_share_image">
+    //       <img src={Share} alt="share"/>
+    //       </div>
+
+    //       <div className="number_of_likes">
+    //       <p className="num_of_shares">12345<span className="likes_pink"> SHARES</span></p>
+    //       </div>
+
+    //  </div>
+  );
 }
